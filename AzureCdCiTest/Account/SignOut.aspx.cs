0bbe77068fca9cc4e9c55d9e5ca4276519ff95e2
Tullio Baruffi <baruffi.tullio@gmail.com>
2017-10-19 12:36:37 +0200

    Primo Commit

---
+++ AzureCdCiTest/Account/SignOut.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AzureCdCiTest.Account
+{
+    public partial class SignOut : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.IsAuthenticated)
+            {
+                // Reindirizza alla pagina iniziale se l'utente è autenticato.
+                Response.Redirect("~/");
+            }
+        }
+    }
+}
